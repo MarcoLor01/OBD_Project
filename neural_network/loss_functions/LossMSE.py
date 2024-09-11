Risolto errore su MNIST, ed errore su flight-price.py relativo alle dimensioni del target-class, presente BUG relativo alla loss e accuracy del flight-price.py, possibile errore sullo scaling delle labels

neural_network/loss_functions/LossMSE.py
@@ -10,14 +10,21 @@ class Mse(Loss):
         self.dinputs = None
 
     def forward(self, output, target_class):
-        target_class = target_class.values.reshape(-1, 1)
-        print("OUTPUT:", output.shape)
-        print("TARGET CLASS:", target_class)
+
+        assert output.shape == target_class.shape, f"Shapes non corrispondenti: output {output.shape}, target {target_class.shape}"
+
+        # Calcolo della MSE
         sample_losses = np.mean((target_class - output) ** 2, axis=-1)
         return sample_losses
 
     def backward(self, dvalues, target_class):
+
+        assert dvalues.shape == target_class.shape, f"Shapes non corrispondenti: dvalues {dvalues.shape}, target {target_class.shape}"
+
         samples = len(dvalues)
-        output = len(dvalues[0])
+        # Numero di features nel layer precedente
+        output = dvalues.shape[1]
+
+        # Gradienti della MSE
         self.dinputs = -2 * (target_class - dvalues) / output
         self.dinputs = self.dinputs / samples

neural_network/metrics_implementations/AccuracyRegression.py
@@ -12,6 +12,6 @@ class AccuracyRegression(Accuracy):
             self.accuracy = np.std(y) / 250
 
     def compare(self, predictions, target_class):
-        target_class = target_class.values.reshape(-1, 1)
+        target_class = target_class.reshape(-1, 1)
         return np.absolute(predictions - target_class) < self.accuracy
 